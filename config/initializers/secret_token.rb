8b28e14f7edaa8d14eab30b9a89c382486c90426
Paul Simpson-Parry <paulsimpsonparry@gmail.com>
2013-02-11 11:53:33 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HollandLaw::Application.config.secret_token = '62e8bf6fa8e26938c0749de34f74a91637eca54f3b3655d61a4d8e3997cd4d0c0e099989e599886cc4dda8703718fc9de9951a74f94ff82a6d795c09a85cf000'
