70617c0a9ab6849871749b701bdcc37501318827
Paul Simpson-Parry <paul.simpsonparry@freelandassociates.com>
2015-01-31 21:16:43 +0000

    Deployment config files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require "bundler/capistrano"
 
-server "198.211.101.221", :web, :app, :db, primary: true
+server "45.56.75.217", :web, :app, :db, primary: true
 
 set :application, "holland_law"
 set :user, "deployer"
@@ -29,7 +29,7 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
@@ -49,47 +49,3 @@ namespace :deploy do
   end
   before "deploy", "deploy:check_revision"
 end
-
-
-
-
-# set :application, 'my_app_name'
-# set :repo_url, 'git@example.com:me/my_repo.git'
-
-# # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# # set :deploy_to, '/var/www/my_app'
-# # set :scm, :git
-
-# # set :format, :pretty
-# # set :log_level, :debug
-# # set :pty, true
-
-# # set :linked_files, %w{config/database.yml}
-# # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# # set :keep_releases, 5
-
-# namespace :deploy do
-
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       # Your restart mechanism here, for example:
-#       # execute :touch, release_path.join('tmp/restart.txt')
-#     end
-#   end
-
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
-
-#   after :finishing, 'deploy:cleanup'
-
-# end
